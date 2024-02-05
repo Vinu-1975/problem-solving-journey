828663a214e8fd3a3501e8c73609ebd186766db5
Vinayakan V S <89218383+Vinu-1975@users.noreply.github.com>
2024-02-05 10:01:25 +0530

    Delete LinkedList/217-contains-duplicate directory

--- LinkedList/217-contains-duplicate/contains-duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hashie = {}
-        for n in nums:
-            hashie[n] = hashie.get(n,0)+1
-            if hashie[n] >= 2:
-                return True
-        
-        return False
