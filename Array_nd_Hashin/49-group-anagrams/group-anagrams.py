ca516e05aed49da92f5c497f63f62877f9e643c0
Vinayakan V S <89218383+Vinu-1975@users.noreply.github.com>
2024-02-05 10:35:23 +0530

    Time: 89 ms (69.46%) | Memory: 20.3 MB (70.17%) - LeetSync

--- Array_nd_Hashin/49-group-anagrams/group-anagrams.py
+++ Array_nd_Hashin/49-group-anagrams/group-anagrams.py
@@ -1,5 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # O (N * log(M))
         hashie = collections.defaultdict(list)
         for word in strs:
             sorted_word = "".join(sorted(word))
