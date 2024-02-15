05ff2247c31cfa4ad175e7d6fa6ccc510d91a1c5
Vinayakan V S <89218383+Vinu-1975@users.noreply.github.com>
2024-02-15 23:18:46 +0530

    Time: 38 ms (88.16%) | Memory: 17.4 MB (98.60%) - LeetSync

--- LinkedList/25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ LinkedList/25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -5,20 +5,61 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        curr = head
-        cnt = 0
-        while cnt < k:
-            if curr is None:
-                return head
-            curr = curr.next
-            cnt += 1
+        # space - O(N/k)
 
-        nxtList = self.reverseKGroup(curr,k)
-        while cnt > 0:
-            temp = head.next
-            head.next = nxtList
-            nxtList = head
-            head = temp
-            cnt -= 1
+        # curr = head
+        # cnt = 0
+        # while cnt < k:
+        #     if curr is None:
+        #         return head
+        #     curr = curr.next
+        #     cnt += 1
+
+        # nxtList = self.reverseKGroup(curr,k)
+        # while cnt > 0:
+        #     temp = head.next
+        #     head.next = nxtList
+        #     nxtList = head
+        #     head = temp
+        #     cnt -= 1
         
-        return nxtList
+        # return nxtList
+
+        # space optimal - O(1)
+        dummy = ListNode(0)
+        dummy.next = head
+        temp = head
+        currHead = head
+        cnt = 1
+        prevList = dummy  # Initialize prevList to dummy node
+        while temp:
+            while cnt < k:
+                if temp.next is None:
+                    break
+                temp = temp.next
+                cnt += 1
+            if temp.next is None and cnt < k:  # If remaining nodes are less than k
+                break
+            nextNode = temp.next
+            temp.next = None
+            newHead = self.revers(currHead)
+            prevList.next = newHead  # Connect the previous sublist to the new head
+            currHead.next = nextNode
+            prevList = currHead
+            currHead = nextNode
+            temp = nextNode
+            cnt = 1
+
+        return dummy.next
+
+    def revers(self,head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+
+
