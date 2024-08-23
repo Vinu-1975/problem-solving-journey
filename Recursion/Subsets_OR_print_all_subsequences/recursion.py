f55c5c4780da3c8d69b3590cf7f65c67a791d4cb
Vinayakan V S <vinayakanvs2003@gmail.com>
2024-08-23 18:16:49 +0530

    added subset (Recursion)

---
+++ Recursion/Subsets_OR_print_all_subsequences/recursion.py
@@ -0,0 +1,22 @@
+
+"""
+
+Time complexity : O(n^2) (2 is the choice to pick or not pick)
+
+"""
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        def f(i,arr):
+            if i>=n:
+                res.append(arr.copy())
+                return
+            arr.append(nums[i]) # pick
+            f(i+1,arr)
+            arr.pop() # not pick
+            f(i+1,arr)
+        
+        f(0,[])
+        return res
