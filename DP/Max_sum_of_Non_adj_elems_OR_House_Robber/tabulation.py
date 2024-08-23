5766c3ddbfd1cfddec61e97e7f5be220e81d977c
Vinayakan V S <vinayakanvs2003@gmail.com>
2024-08-23 20:20:34 +0530

    updated house-robber-i tabulation soln

--- DP/Max_sum_of_Non_adj_elems_OR_House_Robber/tabulation.py
+++ DP/Max_sum_of_Non_adj_elems_OR_House_Robber/tabulation.py
@@ -3,6 +3,28 @@
     # time comeplexity = O(n)
     # space = O(n)
 
+# class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
+
+#         n = len(nums)
+
+#         if n == 1:
+#             return nums[0]
+
+#         dp = [-1] * (n)
+#         dp[0] = nums[0]
+#         dp[1] = max(nums[0],nums[1])
+#         for i in range(2,n):
+#             skip = dp[i-1]
+#             rob = 0
+#             if i-2 >= 0:
+#                 rob = dp[i-2] + nums[i]
+#             dp[i] = max(skip,rob)
+        
+#         return dp[n-1]
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums:
